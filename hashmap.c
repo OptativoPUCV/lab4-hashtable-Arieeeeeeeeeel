83bd9d421844964a2f6e2791d9436a79d1ed8410
Arieeeeeeeeeel <163687992+Arieeeeeeeeeel@users.noreply.github.com>
2024-04-27 19:01:57 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -133,7 +133,7 @@ Pair * nextMap(HashMap * map) {
   if (map == NULL || map->size == 0){
     return NULL;
   }
-  for (int i = map->current; i < map->capacity; i++){
+  for (int i = map->current + 1; i < map->capacity; i++){
     if (map->buckets[i] != NULL && map->buckets[i]->key != NULL){
       map->current = i;
       return map->buckets[i];
